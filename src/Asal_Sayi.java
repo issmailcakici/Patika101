54708b84c70c637b8d3ff1e2064b8c5f01a7b03b
issmailcakici <issmail.cakici@gmail.com>
2023-09-18 21:48:06 +0300

    0-100 arası Asal Sayılar

---
+++ src/Asal_Sayi.java
@@ -0,0 +1,21 @@
+public class Asal_Sayi {
+    public static void main(String[] args) {
+
+        for (int i = 2; i <= 100; i++) {
+            boolean asal = true;
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    asal = false;
+                    break;
+                }
+            }
+            if (asal) {
+                System.out.print(i + " ");
+            }
+        }
+
+
+
+
+    } //Main
+} // Class
